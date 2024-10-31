9bd90800270c8718b1c74300727c08280647e537
nauftis <182428560+Nauftis@users.noreply.github.com>
2024-10-31 19:00:12 +0100

    Use loops to implement 'repeatString', if first test passes, make sure that all others pass too.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, numberToBeRepeated) {
+    if (numberToBeRepeated < 0) {
+        return "ERROR";
+    }
+    let str = "";
+    for (let i = 0; i < numberToBeRepeated; i++) {
+      str += text;
+    }
+    return str;
 };
 
 // Do not edit below this line
