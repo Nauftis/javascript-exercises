b7a4f70fb933c2c7d4069f7715f27e6557d13e81
nauftis <182428560+Nauftis@users.noreply.github.com>
2024-11-01 22:16:44 +0100

    Implement a function that takes an array and some other arguments then removes the other arguments from that array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...allValuesToRemove) {
+    // Array = [1,2,2,3]
+    // toRemove = [2]
+    for (const toRemove of allValuesToRemove) {
+        while (array.indexOf(toRemove) !== -1) {
+            let indexOfToRemove = array.indexOf(toRemove); // index = 1
+            array.splice(indexOfToRemove, 1)
+        }
+        // Remove toRemove from array
+    }
+
+    // Array = [1,3]
+    return array;
 };
 
 // Do not edit below this line
