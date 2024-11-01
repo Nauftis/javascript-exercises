8dd53975ab75582f544a8864a01fc72a92e34f9d
nauftis <182428560+Nauftis@users.noreply.github.com>
2024-11-01 20:18:24 +0100

    mplement a function that takes 2 positive integers and returns the sum of every integer between them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (firstNumber, lastNumber) {
+    if (!Number.isInteger(firstNumber) || !Number.isInteger(lastNumber)) {
+        return "ERROR";
+    }
+    if (firstNumber == NaN || lastNumber == NaN) {
+        return "ERROR";
+    }
+    if (firstNumber < 0 || lastNumber < 0) {
+        return "ERROR";
+    }
+    if (lastNumber < firstNumber) {
+        const switchPlace = lastNumber;
+        lastNumber = firstNumber
+        firstNumber = switchPlace;
+    }
+console.log(firstNumber, lastNumber)
+    let sum = 0;
+    for (let i = firstNumber; i <= lastNumber; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
