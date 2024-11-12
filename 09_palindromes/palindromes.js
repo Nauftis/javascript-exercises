6fd5cf3e2539c4b643c79e28aacad106f46fa933
nauftis <182428560+Nauftis@users.noreply.github.com>
2024-11-12 10:19:28 +0100

    Write function to check if word/sentence is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
-};
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanedString = str
+        .toLowerCase()
+        .split('')
+        .filter((character) => alphanumerical.includes(character))
+        .join('');
+
+    const reversedString = cleanedString.split('').reverse().join('');
+    return cleanedString === reversedString;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
